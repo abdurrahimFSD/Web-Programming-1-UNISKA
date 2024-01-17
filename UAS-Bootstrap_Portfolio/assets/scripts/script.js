631b92003f986f1f3efb36a935e96a600163e2a5
Abdurrahim <abdurrahim22code@gmail.com>
2024-01-17 12:06:43 +0800

    Menambahkan Project UAS Bootstrap Portfolio

---
+++ UAS-Bootstrap_Portfolio/assets/scripts/script.js
@@ -0,0 +1,100 @@
+// NAVBAR JS
+window.onscroll = function() {
+    var header_navbar = document.querySelector(".navbar-area");
+    var sticky = header_navbar.offsetTop;
+
+    if (window.pageYOffset > sticky) {
+        header_navbar.classList.add("sticky");
+    } else {
+        header_navbar.classList.remove("sticky");
+    }
+
+    var backToTo = document.querySelector(".scroll-top");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        backToTo.style.display = "flex";
+    } else {
+        backToTo.style.display = "none";
+    }
+};
+
+var pageLink = document.querySelectorAll('.page-scroll');
+
+pageLink.forEach(elem => {
+    elem.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(elem.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            offsetTop: 1 - 60,
+        });
+    });
+});
+
+function onScroll(event) {
+    var sections = document.querySelectorAll('.page-scroll');
+    var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (var i = 0; i < sections.length; i++) {
+        var currLink = sections[i];
+        var val = currLink.getAttribute('href');
+        var refElement = document.querySelector(val);
+        var scrollTopMinus = scrollPos + 73;
+        if (refElement.offsetTop <= scrollTopMinus && (refElement.offsetTop + refElement.offsetHeight > scrollTopMinus)) {
+            document.querySelector('.page-scroll').classList.remove('active');
+            currLink.classList.add('active');
+        } else {
+            currLink.classList.remove('active');
+        }
+    }
+};
+
+window.document.addEventListener('scroll', onScroll);
+
+
+let navbarToggler = document.querySelector(".navbar-toggler");
+var navbarCollapse = document.querySelector(".navbar-collapse");
+
+document.querySelectorAll(".page-scroll").forEach(e =>
+    e.addEventListener("click", () => {
+        navbarToggler.classList.remove("active");
+        navbarCollapse.classList.remove('show')
+    })
+);
+navbarToggler.addEventListener('click', function() {
+    navbarToggler.classList.toggle("active");
+})
+// NAVBAR JS -END
+
+
+
+
+// SWIPER JS
+const swiperClassNames = [".portfolio-swiper", ".news-swiper"];
+
+swiperClassNames.forEach(swiperClassName => {
+    new Swiper(swiperClassName, {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".btn-next",
+            prevEl: ".btn-prev",
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 15
+            },
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            }
+        }
+    });
+});
+// SWIPER JS -END
